15ca84220108eaaf48660fe51bb0d9d7212a8a38
Keisuke Nakajima <keisukewithaaa@gmail.com>
2020-02-07 16:37:54 +0900

    change date

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,8 +69,8 @@ class PostsController < ApplicationController
 
   def date
     @date = DateTime.parse(params[:date]).to_s
-    @outgos = Budget.where(date: params[:date]).where(budget_type:0).joins(:category).select("categories.*").group("categories.name").order(date: "ASC").sum(:price)
-    @incomes = Budget.where(date: params[:date]).where(budget_type:1).joins(:category).select("categories.*").group("categories.name").order(date: "ASC").sum(:price)
+    @outgos = Budget.where(date: @date).where(budget_type:0).joins(:category).select("categories.*").group("categories.name").order(date: "ASC").sum(:price)
+    @incomes = Budget.where(date: @date).where(budget_type:1).joins(:category).select("categories.*").group("categories.name").order(date: "ASC").sum(:price)
   end
 
   def report
